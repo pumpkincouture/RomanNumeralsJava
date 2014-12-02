f64822df3e7c1775caa5d868ba9f1b0ad409ce78
pumpkincouture <pumpkincouture@gmail.com>
2014-12-02 12:32:17 -0600

    Added method to find key value pair in map

--- src/roman_numerals/RomanNumeralGenerator.java
+++ src/roman_numerals/RomanNumeralGenerator.java
@@ -1,6 +1,8 @@
 package roman_numerals;
 
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by administrator on 12/1/14.
@@ -8,17 +10,18 @@ import java.util.HashMap;
 public class RomanNumeralGenerator {
     public String converter(int number) {
         String romanNumeral = "";
-        HashMap numeralMap = new HashMap();
+        HashMap<Integer, String> numeralMap = new HashMap();
 
         numeralMap.put(4, "IV");
         numeralMap.put(1, "I");
+        numeralMap.put(5, "V");
 
 
         for(int i=0; i < number; i++){
             if (number < 4) {
                 romanNumeral += "I";
             } else {
-                romanNumeral = "IV";
+                romanNumeral = numeralMap.get(number);
             }
         }
         return romanNumeral;
